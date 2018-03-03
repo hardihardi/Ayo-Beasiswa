520ec812ece702b35daf774840a138a66437cbc1
lolitrack <ariaelhamidy@gmail.com>
2018-03-03 18:13:43 +0700

    add sql

---
+++ api_kompress.sql
@@ -0,0 +1,345 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 03, 2018 at 12:13 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `api_kompress`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `beasiswa_kategori`
+--
+
+CREATE TABLE `beasiswa_kategori` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `scholarship_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `beasiswa_kategori`
+--
+
+INSERT INTO `beasiswa_kategori` (`id`, `category_id`, `scholarship_id`, `created_at`, `updated_at`) VALUES
+(1, 2, 1, NULL, NULL),
+(2, 1, 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `judul` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `konten` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `judul`, `konten`, `created_at`, `updated_at`) VALUES
+(1, 'komputer', 'beasiswa komputer', NULL, NULL),
+(2, 'luar negeri', 'beasiswa luar negeri', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facilitators`
+--
+
+CREATE TABLE `facilitators` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama_instansi` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `deskripsi_instansi` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `facilitators`
+--
+
+INSERT INTO `facilitators` (`id`, `nama_instansi`, `deskripsi_instansi`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, 'hacktiv', 'hacktiv belajar', 1, NULL, NULL),
+(2, 'hacktiv8', 'bergerak dibidang komputer', 3, '2018-03-03 04:07:50', '2018-03-03 04:07:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_100000_create_password_resets_table', 1),
+(2, '2018_03_02_091221_create_users_table', 1),
+(3, '2018_03_02_091945_create_scholarships_table', 1),
+(4, '2018_03_02_092026_create_categories_table', 1),
+(5, '2018_03_02_160221_create_beasiswa_kategori_table', 1),
+(6, '2018_03_02_163024_create_facilitator_table', 1),
+(7, '2018_03_02_160132_create_users_beasiswa_table', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scholarships`
+--
+
+CREATE TABLE `scholarships` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama_beasiswa` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama_instantsi` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `quota` int(11) NOT NULL,
+  `konten` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat_gambar` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `masa_berlaku` date NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `scholarships`
+--
+
+INSERT INTO `scholarships` (`id`, `nama_beasiswa`, `nama_instantsi`, `quota`, `konten`, `alamat_gambar`, `masa_berlaku`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, 'beasiswa menjadi', 'hacktiv', 100, 'lalalal', 'lalalala', '2018-03-15', 1, NULL, NULL),
+(2, 'beasiswa menjadi 2', 'hackvtiv', 12, 'lalala', 'lalala', '2018-03-01', 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `alamat` text COLLATE utf8mb4_unicode_ci,
+  `pendidikan` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `telp` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` int(11) NOT NULL DEFAULT '2',
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `nama`, `alamat`, `pendidikan`, `telp`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'ariaelhamidy', 'aria@gmail.com', 'qwerty123', 'aria samudera', 'depok', 'sam', '0989', 1, NULL, NULL, NULL),
+(2, 'ariaelhamidy2', 'aria2@gmail.com', '$2y$10$OxOTc8.6w1utKcs/e6W02uDF7H0gGhB0ktRI3GCYet8oJKukXUi8S', NULL, NULL, NULL, NULL, 2, NULL, '2018-03-02 21:55:26', '2018-03-02 21:55:26'),
+(3, 'ariaganteng', 'aria1@gmail.com', '$2y$10$zKtvuQE3k0yD5czJSGGE0.ItPQMPeQsnydBJHkTe/DyjJBsra.HIK', 'aria samudera', 'depook', 'sma', '089634555083', 1, NULL, '2018-03-03 01:36:43', '2018-03-03 02:13:55');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_scholarship`
+--
+
+CREATE TABLE `user_scholarship` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `scholarship_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `user_scholarship`
+--
+
+INSERT INTO `user_scholarship` (`id`, `user_id`, `scholarship_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `beasiswa_kategori`
+--
+ALTER TABLE `beasiswa_kategori`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `beasiswa_kategori_category_id_foreign` (`category_id`),
+  ADD KEY `beasiswa_kategori_scholarship_id_foreign` (`scholarship_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `facilitators`
+--
+ALTER TABLE `facilitators`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `facilitators_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `scholarships`
+--
+ALTER TABLE `scholarships`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `scholarships_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- Indexes for table `user_scholarship`
+--
+ALTER TABLE `user_scholarship`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_scholarship_user_id_foreign` (`user_id`),
+  ADD KEY `user_scholarship_scholarship_id_foreign` (`scholarship_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `beasiswa_kategori`
+--
+ALTER TABLE `beasiswa_kategori`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `facilitators`
+--
+ALTER TABLE `facilitators`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `scholarships`
+--
+ALTER TABLE `scholarships`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user_scholarship`
+--
+ALTER TABLE `user_scholarship`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `beasiswa_kategori`
+--
+ALTER TABLE `beasiswa_kategori`
+  ADD CONSTRAINT `beasiswa_kategori_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `beasiswa_kategori_scholarship_id_foreign` FOREIGN KEY (`scholarship_id`) REFERENCES `scholarships` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `facilitators`
+--
+ALTER TABLE `facilitators`
+  ADD CONSTRAINT `facilitators_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `scholarships`
+--
+ALTER TABLE `scholarships`
+  ADD CONSTRAINT `scholarships_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_scholarship`
+--
+ALTER TABLE `user_scholarship`
+  ADD CONSTRAINT `user_scholarship_scholarship_id_foreign` FOREIGN KEY (`scholarship_id`) REFERENCES `scholarships` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_scholarship_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

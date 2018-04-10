e3cacc8f7895763ef336be8615314c4858923244
lolitrack <ariaelhamidy@gmail.com>
2018-04-10 10:21:05 +0700

    updated

--- app/Http/Controllers/historyController.php
+++ app/Http/Controllers/historyController.php
@@ -9,7 +9,7 @@ class historyController extends Controller
 {
     public function index($id){
 
-    	$user = User::with(['facilitator', 'scholarship','scholarship.categories','scholarship.facilitator'])->where('id', $id)->get();
+    	$user = User::with(['facilitator', 'scholarship','scholarship.categories','scholarship.facilitator'])->where('id', $id)->first();
     	// dd($user);
     	return response()->json($user->scholarship);
     	
